d6163ef45428ed53e9690ef814e6fd53513608e7
fejxc <1031364436@qq.com>
2021-05-25 23:50:36 +0800

    增加导出合并表

--- checkWorkAttendencce/allStudentCount.py
+++ checkWorkAttendencce/allStudentCount.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
 
-file_name = '0{}.csv'
+#file_name = '0{}.csv'
+file_name = '18-3_20210512_0{}.csv'
 df_list = []
-for i in range(1, 4):
+for i in range(1, 3):
     #data = pd.read_csv(file_name.format(i))
     df_list.append(pd.read_csv(file_name.format(i)))
 df = pd.concat(df_list)
 
 #print(df_list)
-#print(df)
+print(df)
 #print(type(df))
 print(df.value_counts())
 all_date_count = df.value_counts()
@@ -28,11 +29,12 @@ d2 = df['姓名'].value_counts(ascending=True)
 #print(d2)
 
 
-d4.to_csv("18计算机2班考勤统计byName.csv", index=False, header=True, encoding='utf-8')
-d5.to_csv("18计算机2班考勤统计byId.csv", index=False, header=True, encoding='utf-8')
+# d4.to_csv("18计算机2班考勤统计byName.csv", index=False, header=True, encoding='utf-8')
+# d5.to_csv("18计算机2班考勤统计byId.csv", index=False, header=True, encoding='utf-8')
+df.to_csv("18计算机3班考勤统计表汇总.csv", index=False, header=True, encoding='utf-8')
 
-
-
+d4.to_csv("18计算机3班考勤统计byName.csv", index=False, header=True, encoding='utf-8')
+d5.to_csv("18计算机3班考勤统计byId.csv", index=False, header=True, encoding='utf-8')
 
 
 
